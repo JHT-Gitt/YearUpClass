8b3bb5aef6ad37fe4936466cd1bbf417a93b5d60
Jiward H Torres <jiwardtorres@yahoo.com>
2025-05-07 13:09:20 -0700

    new

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Jane", "Doe", "jane.doe@example.com", "female");
+
+        // Print information about the person
+        System.out.println("Person:");
+        System.out.println("Full Name: " + person.getName());
+        System.out.println("Email: " + person.getEmail());
+        System.out.println("Gender: " + person.getGender());
+        System.out.println(); // line break
+
+//        Student student = new Student(
+//                101,                                // ID
+//                "Brian",                            // First name
+//                "Vega-Solano",                      // Last name
+//                "brianvs@example.com",              // Email
+//                "male",                             // Gender
+//                "https://github.com/BrianVegaSol",  // GitHub link
+//                "https://www.linkedin.com/in/brianvs/", // LinkedIn profile
+//                "Crash n' Burn",                    // Coding nickname
+//                "0",                                // Codewars XP (as String)
+//                "img/Avatar_Student_Male.jpg",      // Image URL
+//                "Windows",                          // Device type
+//                0,                                  // Workbook page number
+//                "81"                                // Currently working on (page/task)
+//        );
+        Student student = new Student(
+                100,
+                "Jiward",
+                "Torres",
+                "jiwardtorres@yahoo.com",
+                "Male",
+                "https://github.com/JHT-Gitt",
+                "https://www.linkedin.com/in/jiward-torres/",
+                "FinnDaDog",
+                "0",
+                "https://media.licdn.com/dms/image/v2/D5603AQEVhz2IQLwHCQ/profile-displayphoto-shrink_800_800/B56ZXeZaJqHoAc-/0/1743192957843?e=1752105600&v=beta&t=GZwz1Yn3xmgRTRMM8C5sjJQxmDBBkHoT0e9vLv7XHIE",
+                "Windows",
+                0,
+                "79"
+        );
+        System.out.println("\nStudent:");
+        System.out.println("Full Name: " + student.getFirstName() + " " + student.getLastName());
+        System.out.println("Nickname: " + student.getCodingNickname());
+        System.out.println("GitHuB: " + student.getGithub());
+        System.out.println("Device: " + student.getDeviceType());
+        System.out.println("Currently Working: " + student.getCurrentlyWorkingOn() );
+
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,113 @@
-package PACKAGE_NAME;
+public class Student extends Person {
+    private int id;
+    private String github;
+    private String linkedin;
+    private String codingNickname;
+    private String codewarsxpoints;
+    private String imageUrl;
+    private String deviceType;
+    private int pageNumberWb;
+    private String currentlyWorkingOn;
 
-public class Student {
+//    public Student(String firstname, String lastname, String email, String gender, String github, String linkedin, String nickname,
+//                   String imageurl, String device, String currentlyWorkingOn) {
+//        super(firstname, lastname, email, gender);
+//        this.github = github;
+//        this.linkedin = linkedin;
+//        this.imageUrl = imageurl;
+//        this.deviceType = device;
+//        this.currentlyWorkingOn = currentlyWorkingOn;
+//    }
+    public Student(){
+
+    }
+
+    public Student(int id,String firstName, String lastName,
+                   String email, String gender,
+                   String github, String linkedin,
+                   String codingNickname, String codewarsxpoints,
+                   String imageUrl, String deviceType,
+                   int pageNumberWb, String currentlyWorkingOn) {
+        super(firstName, lastName, email, gender);
+        this.github = github;
+        this.linkedin = linkedin;
+        this.codingNickname = codingNickname;
+        this.codewarsxpoints = codewarsxpoints;
+        this.imageUrl = imageUrl;
+        this.deviceType = deviceType;
+        this.pageNumberWb = pageNumberWb;
+        this.currentlyWorkingOn = currentlyWorkingOn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getCodingNickname() {
+        return codingNickname;
+    }
+
+    public void setCodingNickname(String codingNickname) {
+        this.codingNickname = codingNickname;
+    }
+
+    public String getCodewarsxpoints() {
+        return codewarsxpoints;
+    }
+
+    public void setCodewarsxpoints(String codewarsxpoints) {
+        this.codewarsxpoints = codewarsxpoints;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public int getPageNumberWb() {
+        return pageNumberWb;
+    }
+
+    public void setPageNumberWb(int pageNumberWb) {
+        this.pageNumberWb = pageNumberWb;
+    }
+
+    public String getCurrentlyWorkingOn() {
+        return currentlyWorkingOn;
+    }
+
+    public void setCurrentlyWorkingOn(String currentlyWorkingOn) {
+        this.currentlyWorkingOn = currentlyWorkingOn;
+    }
 }
